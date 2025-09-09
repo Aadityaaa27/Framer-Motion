d367d8908dc34502c634bcf5f2f52e3ea06aa08b
Aadityaaa27 <aadi27003@gmail.com>
2025-09-09 11:28:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+// import React from 'react'
+// import {motion} from 'framer-motion'
+
+// const App = () => {
+//   return (
+//     <div className='text-white  ' >
+//         <h1 className='text-xl text-center mb-2 '>Aditya sharma Fronted developer</h1>
+//         <p className=' text-center font-mono mt-3.5' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente laboriosam laborum a sit eos sequi quo voluptate modi possimus delectus atque sed doloremque odio consequuntur nesciunt odit harum laudantium totam, dignissimos earum! Porro consequatur impedit error repudiandae vitae nulla dolores est rerum, facilis itaque soluta numquam dolor non culpa alias velit molestiae repellendus iste a blanditiis corrupti? Eos, quia asperiores. Quisquam doloremque tenetur eum animi laboriosam. Ipsum molestias odit, quidem pariatur optio placeat maiores dolores recusandae sed soluta magni magnam architecto quas, accusantium at in delectus iste neque tempora! Facere tempore illo quod cumque eos officia, placeat, minus perspiciatis neque iusto minima sunt veritatis necessitatibus! Magni fuga numquam aliquam quae, odit consequatur sint, sequi ex pariatur sit, quod beatae debitis nam! Doloribus, odio. Vel, labore itaque alias nemo nobis inventore praesentium.<br></br> Eaque aliquam, porro dignissimos sapiente impedit quos quaerat illo. Vel, aperiam itaque maiores magni nostrum veritatis cum obcaecati nesciunt. Saepe eligendi neque iure accusantium repellat veniam. Eos obcaecati reprehenderit error nisi deleniti sed magni eius tenetur. Sed quidem animi voluptates provident tempore aut quibusdam nobis quo debitis, vero mollitia accusantium nam in iste earum! Modi ipsa tempore, assumenda est minima a ab non similique distinctio facilis neque aliquam, ducimus mollitia perspiciatis doloribus numquam magnam ratione incidunt minus. Eveniet magni sint eaque quasi molestiae inventore tempore neque ullam modi, aliquid officiis expedita cupiditate exercitationem nulla itaque,<br></br> mollitia recusandae accusantium corporis harum distinctio animi nemo quas. Impedit voluptatem laboriosam eius! Id ex inventore nobis voluptas voluptates a illum ullam nesciunt voluptate, maxime porro eligendi amet consequatur beatae optio cupiditate quo sed praesentium dignissimos, quis consequuntur officiis distinctio alias? Nam labore ad laudantium vitae laborum cumque praesentium! Dolorem minus rerum natus nesciunt sunt magnam suscipit in optio doloremque accusantium sequi laudantium esse consequuntur quidem, velit atque. Voluptatem, nisi fugit beatae ducimus sunt repudiandae delectus saepe inventore explicabo excepturi voluptatibus, nesciunt eligendi. Doloribus ipsam nam impedit dolorum, aliquid fugiat sunt est? Est mollitia nihil, qui aliquam veritatis vero et neque voluptates suscipit explicabo doloribus?<br></br><br></br><br></br> Error, accusantium numquam, quis excepturi laudantium cum velit placeat dolorum voluptatibus soluta blanditiis sequi hic deserunt. Reiciendis nesciunt nostrum est ipsam omnis, quasi odio dolores culpa aut illo commodi perferendis consectetur quos cupiditate, veritatis, excepturi necessitatibus? Minima nam quia consectetur neque illo enim ratione molestiae eveniet iusto qui a quod rem saepe ipsa, adipisci animi corporis accusantium sunt? Id, expedita nisi error doloribus sit a est nesciunt suscipit nobis nam excepturi, perferendis quis quo explicabo quos quod, eum nostrum. Quaerat ad quasi accusamus perferendis debitis esse repellendus reiciendis vel sunt! Ducimus magnam ipsa fugit harum quasi aperiam ipsum error modi! Incidunt similique maxime rerum exercitationem quis odit deleniti eos ducimus optio provident dolorum autem voluptatem, adipisci natus, tempore fugiat, recusandae praesentium non consequuntur. Voluptatem corrupti quasi laborum eligendi magni atque suscipit ipsam accusantium aliquam. Eaque temporibus perspiciatis doloremque eum ea veniam? Corrupti quod, ipsum nihil saepe assumenda, asperiores, eligendi facilis esse harum voluptas molestias! Assumenda, blanditiis ut officia dolorum sunt architecto veniam magni, at est odit minus placeat harum debitis quae neque nisi accusantium quos! Corporis ad vel minima temporibus ducimus sapiente, consequatur ullam rem cumque libero aperiam qui explicabo delectus unde illo quidem quibusdam, molestiae totam! <br></br>Ex quasi numquam labore aspernatur ullam alias eveniet tempora facilis animi in voluptas magni quisquam, dolorem adipisci tenetur vel maiores dicta minus hic ipsum esse. Aspernatur excepturi et fugiat! Voluptates veniam nemo consectetur minima accusantium enim non officia facere alias ut? Impedit molestias nobis dolorem architecto expedita esse nulla magni non qui voluptatum, obcaecati ipsum eius? Voluptates ipsam nam sint quibusdam atque, vero, reprehenderit ipsum voluptatem voluptate ratione autem, saepe assumenda dolores omnis fuga provident.<br></br> Ea possimus accusamus atque fugiat fugit, necessitatibus itaque modi tempora corrupti dolores, praesentium quae, similique adipisci quibusdam? Eius praesentium impedit possimus obcaecati perferendis quia, culpa, harum nisi repudiandae dolores tempore quod enim sapiente odio ipsum! Repellat fugit, corporis iure quia reiciendis nulla commodi? Mollitia, necessitatibus laborum! Beatae hic possimus at maiores voluptatum modi illum dolore ad placeat ipsam, excepturi officiis itaque sapiente fugit praesentium! Omnis nostrum in dolorem laborum dolores, harum laudantium vel aperiam repudiandae, iste magnam quae, et enim ut iure facilis praesentium eos nulla non dolor itaque. Corporis, alias nam. Fugit velit qui quidem aliquam.</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import {motion} from 'framer-motion'
+
+// const App = () => {
+//   return (
+//     <div >
+//       <motion.div className='container'
+//         drag
+//         dragConstraints={{
+//           left: 0,
+//           top: 0,
+//           right:600,
+//           bottom:400,
+//         }}
+//         whileDrag={{
+//           scale: 0.5,
+//         }}
+//       >
+         
+//       </motion.div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const App = () => {
+  return (
+    <div >
+      <motion.div className='container'
+      // initial={{
+      //   x:0,
+      //   y:0,
+      // }}
+      // animate={{
+      //   x:[0,900,900,0,0],
+      //   y:[0,0,500,500,0],
+      // }}
+
+       initial={{ filter: "blur(10px)" }}
+      animate={{ filter: "none" }}
+      transition={{
+        duration:3,
+        delay:1,
+      }}
+     
+      >
+         
+      </motion.div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import {motion} from 'framer-motion'
+
+// const App = () => {
+//   return (
+//     <div >
+//       <motion.div className='container'
+//       initial={{
+//         x:1700,
+//         rotate:0,
+//       }}
+//       animate={{
+//         x:500,
+//         // rotate:360,
+//       }}
+//       transition={{
+//         duration:2,
+//         delay:1,
+//       }}
+//       >
+         
+//       </motion.div>
+//     </div>
+//   )
+// }
+
+// export default App
